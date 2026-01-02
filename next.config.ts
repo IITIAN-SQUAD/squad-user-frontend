48c6aa0d0bb6a381d5ada2490ac5a8050b1f6924
sachin2496 <sachin.kumar.cse20@itbhu.ac.in>
2026-01-02 17:03:51 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      root: path.resolve(__dirname),
-    },
-  },
   async rewrites() {
     return [
       {
